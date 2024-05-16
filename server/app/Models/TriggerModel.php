f2cd21d9d75a14bfae4ac5f8118225af8a1ccf43
ILIA ILIA(Ilia Litvinenko) <lila96611@gmail.com>
2024-05-16 17:08:45 +0300

    Create Trigger table and Bot_Trigger table

---
+++ server/app/Models/TriggerModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TriggerModel extends Model
+{
+    use HasFactory;
+}
